463c746ff29b94f998a289619321e4a854e5354d
Hyperinit <tobias.roth@tu-ilmenau.de>
2016-11-23 06:21:16 +0100

    Step 1 in Project Passants + Added Score to Screen
    "Passants" are now moving from the right side to the left. Nothing special yet, but nice to have.
    In the top left corner should occur the actual score

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject passant;
+    public Vector3 spawnValues;
+
+    public float spawnWait;
+    public float startWait;
+
+    public GUIText scoreText;
+    private int score;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnPassasnts());
+    }
+
+    IEnumerator SpawnPassasnts()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(passant, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(startWait);
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
---
+++ Assets/Scripts/MovePassant.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePassant : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * -speed;
+    }
+}
