2f80afb687233a383d86f825a9fbde38401b0cbc
Hyp <tobias.roth@live.com>
2016-12-01 15:39:44 +0100

    Added ice and ice can stitch to everyother object
    2 spheres are resembling 2 ice balls. They stitch by contact to the other object

---
+++ Assets/Scripts/IceAttachWaffle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceAttachWaffle : MonoBehaviour {
+
+    private GameController gameController;
+
+	void Start () {
+
+
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        gameController = gameControllerObject.GetComponent<GameController>();
+	}
+	
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.CompareTag("waffle"))
+        {
+            transform.SetParent(other.transform);
+            //transform.parent = other.transform;
+            gameController.AddScore(1);
+
+        }
+    }
+}
