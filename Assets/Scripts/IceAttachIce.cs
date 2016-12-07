252b97f2030e96f2778c03545a0c1aecbdde35f1
roamer543 <student@berenger.rz.tu-ilmenau.de>
2016-12-07 15:26:51 +0100

    update on 07.12.2016

---
+++ Assets/Scripts/IceAttachIce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceAttachIce : MonoBehaviour
+{
+
+    private GameController gameController;
+
+    void Start()
+    {
+
+
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        gameController = gameControllerObject.GetComponent<GameController>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ice"))
+        {
+            transform.SetParent(other.transform);
+            //transform.parent = other.transform;
+            gameController.AddScore(1);
+
+        }
+    }
+}
