b78fae94a7b350a89c83a98112147ea886001970
hyp <tobias.roth@tu-ilmenau.de>
2016-12-08 14:56:20 +0100

    Ordner angepasst

---
+++ Assets/Scripts/WaffleBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaffleBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void DestroyWaffle()
+    {
+        Destroy(gameObject,0);
+    }
+}
