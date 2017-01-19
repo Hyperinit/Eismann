2e7d442b553b8342d17a2a70310ce4f518129cc3
Hyperinit <tobias.roth@tu-ilmenau.de>
2017-01-19 01:53:46 +0100

    Animation switch working. Speechbubbles will fade out bevor destroy

--- Assets/Scripts/SpeechbubbleBehavior.cs
+++ Assets/Scripts/SpeechbubbleBehavior.cs
@@ -12,9 +12,13 @@ public class SpeechbubbleBehavior : MonoBehaviour {
     private Vector3[] orderPositions;
     private GameObject[] iceBalls;
 
+    //Fadeing out Speed
+    float fadePerSec = 0.5f;
+    bool fadeOutbool;
 
     // Use this for initialization
     void Start () {
+        fadeOutbool = false;
         GameObject gameControllerObject = GameObject.FindWithTag("GameController");
         gameController = gameControllerObject.GetComponent<GameController>();
         order=gameController.getOrder();
@@ -27,31 +31,50 @@ public class SpeechbubbleBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (fadeOutbool)
+            fadeOut();
+    }
 
-    void placeOrderInBubble()
+    void placeOrderInBubble() //switch could be used here
     {
         for(int i=0;i<order.GetLength(0);i++)
         {
             if(order[i] == 0)
             {
-                Instantiate(erdbeere, transform.position + orderPositions[i], transform.rotation).transform.parent = transform;
+                iceBalls[i] = Instantiate(erdbeere, transform.position + orderPositions[i], transform.rotation); //this could be a one liner, but this way it is way easier to implement a fading out method
+                iceBalls[i].transform.parent = transform;
             }
             else if(order[i] == 1)
             {
-                Instantiate(schoko, transform.position + orderPositions[i], transform.rotation).transform.parent = transform;
+                iceBalls[i] = Instantiate(schoko, transform.position + orderPositions[i], transform.rotation);
+                iceBalls[i].transform.parent = transform;
             }
             else if (order[i] == 2)
             {
-                Instantiate(vanille, transform.position + orderPositions[i], transform.rotation).transform.parent = transform;
+                iceBalls[i] = Instantiate(vanille, transform.position + orderPositions[i], transform.rotation);
+                iceBalls[i].transform.parent = transform;
             }
         }
     }
 
-    public void destroySpeechbubble()
+    void fadeOut() //will make the object and all iceballs slowly disappear IT WONT DESTROY THEM just working with the color
     {
-        Destroy(gameObject, 0);
+        var material = GetComponent<Renderer>().material;
+        var color = material.color;
+        material.color = new Color(color.r, color.g, color.b, color.a - (fadePerSec * Time.deltaTime));
+        for (int i = 0;i < iceBalls.GetLength(0);i++)
+        {
+            material = iceBalls[i].GetComponent<Renderer>().material;
+            color = material.color;
+            material.color = new Color(color.r, color.g, color.b, color.a - (fadePerSec * Time.deltaTime));
+        }
+        
+    }
+
+    public void destroySpeechbubble() //bye bye Speechbubbles we will miss you :'(
+    {
+        fadeOutbool = true;  //enable fadeOut() in Update()
+        Destroy(gameObject, 5); //destroy this gameObject and it childs after time
         Debug.Log("Speechbubble wird zerstört");
     }
 }
