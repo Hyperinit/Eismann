d3b6748cbceaeeafbeff12c4db4a3356a09c7754
Hyp <tobias.roth@live.com>
2017-01-26 11:53:24 +0100

    Pull and Set functions for Icewaffle

--- Assets/Scripts/WaffleBehavior.cs
+++ Assets/Scripts/WaffleBehavior.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class WaffleBehavior : MonoBehaviour {
+    int[] order;
+    int[] iceballs;
 
 	// Use this for initialization
 	void Start () {
         transform.RotateAround(transform.position, transform.up, 180f);//rotate y-axis 180 at spawn
+        iceballs = new int[] { 0,0,0,0,0};
     }
 	
 	// Update is called once per frame
@@ -15,5 +18,15 @@ public class WaffleBehavior : MonoBehaviour {
     public void DestroyWaffle()
     {
         Destroy(gameObject,0);
+    }
+
+    public void GiveOrder(int[] nOrder)
+    {
+        order = nOrder;
+    }
+
+    public int[] pullIceballs()
+    {
+        return iceballs;
     }
 }
