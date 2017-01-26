64652a4c1d02914a61428d835fdee304d2e8389f
Hyperinit <tobias.roth@tu-ilmenau.de>
2017-01-26 04:00:28 +0100

    new SpawnPassants Algo not tested yet

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : MonoBehaviour {
     private int counterPassants;
     private Vector3[] spawnPositionsPassants;
     private float[] spawnRotationPassants;
+    private int []laneAssignments;
 
     public GUIText scoreText;
     private int score;
@@ -37,12 +38,14 @@ public class GameController : MonoBehaviour {
 
     void Start()
     {
-        spawnPositionsPassants = new Vector3[] { new Vector3(10f,1f,3.3f),new Vector3(10f,1f,5.3f), new Vector3(10f,1f,6.2f), new Vector3(10f, 1f, 7.4f) , new Vector3(10f, 1f, 8.6f) };
+        spawnPositionsPassants = new Vector3[] { new Vector3(10f,1f,3.3f),new Vector3(10f,1f,5.3f), new Vector3(10f,1f,6.2f), new Vector3(10f, 1f, 7.4f) , new Vector3(10f, 1f, 8.6f) };//diese drei Arrays müssen die !gleiche! Länge haben
         spawnRotationPassants =new float[]{ 0f,-2.862f,0f, 0f, 0f };
+        laneAssignments = new int[] { 0, 0, 0, 0, 0 };
+
         score = 0;
         UpdateScore();
-        StartCoroutine(SpawnPassants());
-        //PassantsTest();
+        //StartCoroutine(SpawnPassants());
+        StartCoroutine(PassantsTest());
 
         waffleIsComplete = false;
         //waffleBehaviorScript = waffleClone.GetComponent(WaffleBehavior);
@@ -57,22 +60,77 @@ public class GameController : MonoBehaviour {
 
     }
 
-    void PassantsTest()
+    int GiveFreeLane()
     {
+        int freeLane = (int)(Random.value * spawnPositionsPassants.GetLength(0)); //Startposition der Suche
+        int searchedLanes = 0;
+        int lanes = spawnPositionsPassants.GetLength(0);
+        int run = 0;
+        //for (; freeLane < spawnPositionsPassants.GetLength(0); freeLane++)
+        while (true)
+        {
+            for(;searchedLanes<lanes;searchedLanes++)
+            {
+                if(freeLane==lanes) //wir zählen nicht beginnend bei 0, daher überprüfen, ob wir noch im gültigen Bereich sind
+                {
+                    freeLane = 0;
+                }
+
+                if (laneAssignments[freeLane]==run)
+                {
+                    return freeLane;
+                }
+                else
+                {
+                    freeLane++;
+                }
+            }
+            run++;//wenn es nicht anders geht, dann setzen wir halt mehr als einen Passanten auf eine Lane
+        }
+        return -1;
+    }
+
+    IEnumerator PassantsTest()
+    {
+        while(true)
+        {
+            if (maxPassants > counterPassants)
+            {
+                int freeLane = GiveFreeLane();//Auf welcher Lane ist gerade wenig los?
+
+                if (Random.value > 0.5)//Entscheidung, ob Passant links oder rechts startet
+                {
+                    spawnPositionsPassants[freeLane].x *= -1;//ob die if Abfrage true oder false ist, definiert nicht, ob der Passant links oder rechts spawnt, sondern nur ob er an der gleichen Stelle wie der vorherige oder auf der anderen Seite spawnt
+                    spawnRotationPassants[freeLane] = 180 - spawnRotationPassants[freeLane];
+                }
+
+                var tempPassant = (GameObject)Instantiate(passant, spawnPositionsPassants[freeLane], Quaternion.identity);
+                MovePassant MovePassantScript = (MovePassant)tempPassant.GetComponent(typeof(MovePassant));
+                MovePassantScript.AddRotation(spawnRotationPassants[freeLane]);
+
+                counterPassants++;
+                laneAssignments[freeLane]++;//jetzt ist einer mehr unterwegs :)
+            }
+
+        }
+
         for (int i = 0; i < spawnPositionsPassants.GetLength(0); i++)
         {
-            if(Random.value>0.5)
+            if (Random.value > 0.5)
             {
                 spawnPositionsPassants[i].x *= -1;
-                spawnRotationPassants[i] = 180-spawnRotationPassants[i];
+                spawnRotationPassants[i] = 180 - spawnRotationPassants[i];
             }
             Quaternion spawnRotation = Quaternion.identity;
 
             var tempPassant = (GameObject)Instantiate(passant, spawnPositionsPassants[i], Quaternion.identity);
             MovePassant MovePassantScript = (MovePassant)tempPassant.GetComponent(typeof(MovePassant));
             MovePassantScript.AddRotation(spawnRotationPassants[i]);
-           //MovePassantScript.path = i;
+            //MovePassantScript.path = i;
+            //yield return new WaitForSeconds(startWait);
+            yield return new WaitForSeconds(0.5f);
         }
+        //yield return null;
     }
 
     IEnumerator SpawnPassants()
@@ -89,7 +147,7 @@ public class GameController : MonoBehaviour {
                 counterPassants++;
                 Debug.Log("Passantenanzahl: "+counterPassants);
             }
-            yield return new WaitForSeconds(startWait);
+            yield return new WaitForSeconds(startWait);//TODO ersetzen durch spawnWait
         }
     }
 
